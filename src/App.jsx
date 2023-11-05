014db43192a8a8d018464fbb2bfc7d78fc7708a8
Maximiliano Chavez <104655710+MaxiChavez@users.noreply.github.com>
2023-11-05 13:28:28 +0100

    Se agrega footer sin estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./pages/home/Home";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
       <Home />
+      <Footer />
     </>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import logoFooter from "../../assets/LogoFooter1.png";
+
+const Footer = () => {
+  return (
+    <div>
+      <img src={logoFooter} alt="Logo empresarial" />
+    </div>
+  );
+};
+
+export default Footer;
