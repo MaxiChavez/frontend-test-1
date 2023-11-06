bd8f8516b7454e09955fccfec38419f6fcc17964
Maximiliano Chavez <104655710+MaxiChavez@users.noreply.github.com>
2023-11-06 17:20:36 +0100

    Se corrigen estilos, botones

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,14 +3,13 @@ import "./Home.css";
 import HomeHeader from "./Header/HomeHeader";
 import HomeContent from "./Content/HomeContent";
 import Footer from "../../components/footer/Footer";
-import HomeBottom from "./Bottom/HomeBottom";
 
 const Home = () => {
   return (
     <>
       <HomeHeader />
       <HomeContent />
-      <HomeBottom />
+
       <Footer />
     </>
   );
